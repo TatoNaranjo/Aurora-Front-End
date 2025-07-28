2fdacd702091aa66d49b6dc588c06f4d75743005
TatoNaranjo <naranjosa2004@gmail.com>
2025-07-28 10:27:53 -0500

    feat: implement Diagnosis page

---
+++ src/pages/Diagnosis/Diagnosis.tsx
@@ -0,0 +1,9 @@
+import './Diagnosis.css';
+export default function Diagnosis() {
+  return (
+    <div>
+      <h1>Diagnosis Page</h1>
+      <p>This is the Diagnosis page content.</p>
+    </div>
+  );
+}
